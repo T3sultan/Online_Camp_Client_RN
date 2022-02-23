94778d4ef5c46466817d03298a4954ed1e06dc61
“T3sultan” <incognito.soul011@gmail.com>
2022-02-23 20:57:51 +0600

    folder structure

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    EncodedSansBold: require("./assets/fonts/EncodeSans-Bold.ttf"),
+    EncodedSansRegular: require("./assets/fonts/EncodeSans-Regular.ttf"),
+    MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf"),
+    MontserratRegular: require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "EncodedSansBold" }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/main/BookMark.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BookMark = () => {
+  return (
+    <View>
+      <Text>BookMark</Text>
+    </View>
+  );
+};
+
+export default BookMark;
+
+const styles = StyleSheet.create({});
