8924af7e362c9d51fc2f8e186daea61b42bea642
“T3sultan” <incognito.soul011@gmail.com>
2022-02-23 21:10:57 +0600

    custom screen width & height folder metrics

---
+++ src/theme/metrics.js
@@ -0,0 +1,15 @@
+import { Dimensions, platform } from "react-native";
+import { scale } from "react-native-size-matters";
+
+const { width, height } = Dimensions.get("window");
+
+const metrics = {
+  header: scale(60),
+  base: scale(20),
+  halfBase: scale(10),
+  doubleBase: scale(40),
+  screenWidth: width,
+  screenHeight: height,
+};
+
+export default metrics;
